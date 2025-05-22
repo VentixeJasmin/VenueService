546cec29f5e56cf951663dfaf50a56767ca433ef
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-05-22 13:10:44 +0200

    Add project files.

---
+++ Presentation/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Presentation.Models; 
+
+namespace Data.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<VenueEntity> Events { get; set; }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,23 @@
+using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Presentation.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<VenueService>(); 
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("VenueDatabaseConnection")));
+
+var app = builder.Build();
+
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
